f9a8847342663cdc2248823f471d700f2bdde18a
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2018-04-09 10:24:21 +0100

    Rename componentˆ

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Token from '@/components/Token'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Token',
+      component: Token
     }
   ]
 })
